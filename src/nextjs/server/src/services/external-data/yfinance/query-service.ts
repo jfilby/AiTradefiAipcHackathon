26c1cc7bf6ad85b2d1f443c808bbbc5e86e2c03c
jfilby <jason.filby@gmail.com>
2025-12-03 09:07:01 +0200

    Validate that context data exists.

--- src/nextjs/server/src/services/external-data/yfinance/query-service.ts
+++ src/nextjs/server/src/services/external-data/yfinance/query-service.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { YFinanceInstrumentContext } from './types'
 import { YFinanceFinModel } from '@/models/yfinance-models/yfinance-fin-model'
 import { YFinanceQuoteModel } from '@/models/yfinance-models/yfinance-quote-model'
+import { CustomError } from '@/serene-core-server/types/errors'
 
 // Models
 const yFinanceQuoteModel = new YFinanceQuoteModel()
@@ -17,21 +18,38 @@ export class YFinanceQueryService {
           prisma: PrismaClient,
           instrumentId: string) {
 
-    // Get context records
+    // Debug
+    const fnName = `${this.clName}.getContext()`
+
+    // Get quote
     const yFinanceQuote = await
             yFinanceQuoteModel.getByUniqueKey(
               prisma,
               instrumentId)
 
+    if (yFinanceQuote == null) {
+      throw new CustomError(`${fnName}: yFinanceQuote == null`)
+    }
+
+    // Get financials
     const yFinanceFins = await
             yFinanceFinModel.filter(
               prisma,
               instrumentId)
 
+    if (yFinanceFins.length === 0) {
+      throw new CustomError(`${fnName}: yFinanceFins.length === 0`)
+    }
+
+    // Get charts
     const yFinanceCharts = await
             yFinanceFinModel.filter(
               prisma,
               instrumentId)
+
+    if (yFinanceCharts.length === 0) {
+      throw new CustomError(`${fnName}: yFinanceCharts.length === 0`)
+    }
 
     // Set context
     const yFinanceInstrumentContext: YFinanceInstrumentContext = {
