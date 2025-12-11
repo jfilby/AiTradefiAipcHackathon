2b5ad0240340c83e47d39dde4a1bd57cd715c390
jfilby <jason.filby@gmail.com>
2025-12-11 13:31:30 +0200

    Add a few useful consts to ServerTestTypes.

--- src/nextjs/server/src/types/server-test-types.ts
+++ src/nextjs/server/src/types/server-test-types.ts
@@ -1,5 +1,12 @@
 export class ServerTestTypes {
 
+  // Account emails
   static adminUserEmail = 'jason.filby@gmail.com'
   static regularTestUserEmail = 'test.user1@aitradefi.com'
+
+  // Analyses
+  static testAnalysisVersion = '1.0.0'
+
+  // Stock symbols
+  static nvdaSymbol = 'NVDA'
 }
